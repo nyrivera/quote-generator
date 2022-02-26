3b464a30baedc67b5509d1795ae5f3db9e3f187c
Nelson Rivera <nyrivera@student.rtc.edu>
2022-02-25 16:36:15 -0800

    Deleted loader

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+function complete() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
 //filter quote to use
 function newQuote() {
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
